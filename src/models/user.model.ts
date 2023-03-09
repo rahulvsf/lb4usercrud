9748d415cd6425fb4815ae9f002c871f67dd7c92
Rahul Vijay <rahul.vijay@sourcefuse.com>
2023-03-09 10:09:19 +0530

    in memory db

---
+++ src/models/user.model.ts
@@ -0,0 +1,71 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  fname: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  mname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  role: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  created: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
