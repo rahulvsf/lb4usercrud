8bd7c2573668e714878aff3abb0cd62ed9b00cbb
Rahul Vijay <rahul.vijay@sourcefuse.com>
2023-03-09 17:18:10 +0530

    sequence handler

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,8 +1,30 @@
-import {MiddlewareSequence, RequestContext} from '@loopback/rest';
+import {inject} from '@loopback/core';
+import {
+  FindRoute,
+  InvokeMethod,
+  parseOperationArgs,
+  Reject,
+  RequestContext,
+  RestBindings,
+  Send,
+  SequenceActions,
+  SequenceHandler,
+} from '@loopback/rest';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export class MySequence extends MiddlewareSequence {
+export class MySequence implements SequenceHandler {
+  constructor(
+    @inject(SequenceActions.FIND_ROUTE)
+    protected findRoute: FindRoute,
+    @inject(RestBindings.SequenceActions.PARSE_PARAMS)
+    protected invoke: InvokeMethod,
+    @inject(RestBindings.SequenceActions.SEND)
+    public send: Send,
+    @inject(RestBindings.SequenceActions.REJECT)
+    public reject: Reject,
+  ) {}
+
   log(message: string) {
     console.log(message);
   }
@@ -14,17 +36,23 @@ export class MySequence extends MiddlewareSequence {
         process.env.ALLOWED_ORIGIN as string,
       )
     ) {
+      const {request, response} = context;
+
+      const route = this.findRoute(request);
+      const args = await parseOperationArgs(request, route);
+
       const startDate = new Date();
       this.log('STARTING AT ' + startDate.toLocaleTimeString());
       this.log('HOST: ' + context.request.headers.host);
       this.log('REFERER: ' + context.request.headers.referer);
       this.log('USER AGENT: ' + context.request.headers['user-agent']);
       this.log('IP: ' + context.request.socket.remoteAddress);
-      await super.handle(context);
+      const result = await this.invoke(route, args);
+      this.send(response, result);
       const endDate = new Date();
       this.log('CLOSING AT ' + endDate.toLocaleTimeString());
     } else {
-      context.response.status(403).send('INVALID ORIGIN');
+      this.reject(context, {name: '403', message: 'Not allowed'});
     }
   }
 }
