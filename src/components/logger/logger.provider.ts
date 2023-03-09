8e8525d26b9a3ce91ce9215ba6267800285a8a64
Rahul Vijay <rahul.vijay@sourcefuse.com>
2023-03-09 16:52:06 +0530

    winston logger

--- src/components/logger/logger.provider.ts
+++ src/components/logger/logger.provider.ts
@@ -1,7 +1,39 @@
 import {Provider, ValueOrPromise} from '@loopback/context';
+import winston from 'winston';
+import {LogTypes} from './logger.keys';
 
-export class LoggerProvider implements Provider<string> {
-  value(): ValueOrPromise<string> {
-    return 'Logger Provider';
+type LoggerFunction = (level: number, message: string) => void;
+
+const loggerOptions = {
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({filename: 'error.log', level: 'error'}),
+    new winston.transports.File({filename: 'combined.log'}),
+  ],
+};
+
+export class LoggerProvider implements Provider<LoggerFunction> {
+  winston: winston.Logger;
+
+  constructor() {
+    this.winston = winston.createLogger(loggerOptions);
+  }
+
+  value(): ValueOrPromise<LoggerFunction> {
+    return (level: number, message: string) => {
+      this.loggerAction(level, message);
+    };
+  }
+
+  loggerAction(level: number, message: string) {
+    switch (level) {
+      case LogTypes.ERROR:
+        this.winston.error(message);
+        break;
+      case LogTypes.INFO:
+        this.winston.info(message);
+        break;
+    }
   }
 }
