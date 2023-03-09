33b013050c496661c86e276f7bcf8de2431088bc
Rahul Vijay <rahul.vijay@sourcefuse.com>
2023-03-09 12:38:45 +0530

    log sequence

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,20 @@
-import {MiddlewareSequence} from '@loopback/rest';
+import {MiddlewareSequence, RequestContext} from '@loopback/rest';
 
-export class MySequence extends MiddlewareSequence {}
+export class MySequence extends MiddlewareSequence {
+  log(message: string) {
+    console.log(message);
+  }
+
+  async handle(context: RequestContext) {
+    const startDate = new Date();
+    this.log('STARTING AT ' + startDate.toLocaleTimeString());
+    this.log('HOST: ' + context.request.headers.host);
+    this.log('REFERER: ' + context.request.headers.referer);
+    this.log('USER AGENT: ' + context.request.headers['user-agent']);
+    this.log('IP: ' + context.request.socket.remoteAddress);
+    await super.handle(context);
+    const endDate = new Date();
+    this.log('CLOSING AT ' + endDate.toLocaleTimeString());
+    this.log('AFTER REQUEST');
+  }
+}
