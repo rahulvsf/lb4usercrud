8534a0037d7279c507dedde6f8064903a3ca2afd
Rahul Vijay <rahul.vijay@sourcefuse.com>
2023-03-09 12:46:45 +0530

    intercept request

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,4 +1,6 @@
 import {MiddlewareSequence, RequestContext} from '@loopback/rest';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export class MySequence extends MiddlewareSequence {
   log(message: string) {
@@ -6,15 +8,22 @@ export class MySequence extends MiddlewareSequence {
   }
 
   async handle(context: RequestContext) {
-    const startDate = new Date();
-    this.log('STARTING AT ' + startDate.toLocaleTimeString());
-    this.log('HOST: ' + context.request.headers.host);
-    this.log('REFERER: ' + context.request.headers.referer);
-    this.log('USER AGENT: ' + context.request.headers['user-agent']);
-    this.log('IP: ' + context.request.socket.remoteAddress);
-    await super.handle(context);
-    const endDate = new Date();
-    this.log('CLOSING AT ' + endDate.toLocaleTimeString());
-    this.log('AFTER REQUEST');
+    if (
+      context.request.headers.host?.includes(
+        process.env.ALLOWED_ORIGIN as string,
+      )
+    ) {
+      const startDate = new Date();
+      this.log('STARTING AT ' + startDate.toLocaleTimeString());
+      this.log('HOST: ' + context.request.headers.host);
+      this.log('REFERER: ' + context.request.headers.referer);
+      this.log('USER AGENT: ' + context.request.headers['user-agent']);
+      this.log('IP: ' + context.request.socket.remoteAddress);
+      await super.handle(context);
+      const endDate = new Date();
+      this.log('CLOSING AT ' + endDate.toLocaleTimeString());
+    } else {
+      context.response.send('INVALID ORIGIN');
+    }
   }
 }
