723e0604fcd3a6a8d5346d9e58ab0c18f30fdfc7
Rahul Vijay <rahul.vijay@sourcefuse.com>
2023-03-15 15:46:32 +0530

    for review

--- src/application.ts
+++ src/application.ts
@@ -23,6 +23,51 @@ export {ApplicationConfig};
 export class UserappApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
+  // REVIEW:
+  /**
+   *
+   * 1. LB4 simple crud with in-memory connector:
+   * datasources/db.datasource.ts
+   * data/db.json
+   * controllers/user.controller.js
+   *
+   * 2. Customize boot options to load from different path
+   * bootercontrollers/*
+   * modified: this.bootOptions in application.ts
+   *
+   * 3. .env file with ALLOWED_ORIGIN added
+   * added a check in sequence.ts to check origin
+   *
+   * 4. Logging with winston
+   * created a new component: components/logger
+   * with: types, keys, provider and component file
+   * added to sequence to log at console + 2 diff .log files
+   *
+   * 5. Postgres Database connector for Roles & Customers
+   * datasources/postgres.datasource.ts
+   *
+   * 6. Relation between users, roles and customers
+   * hasOne() relation in --> models/user.model.ts
+   *
+   * 7. Simple REST Connector --> https://github.com/rahulvsf/crudconnectors
+   *
+   * 8. JWT middleware
+   * gets cookie, added auth header to request
+   * middleware/jwtheader.ts
+   *
+   * 9. Authentication package
+   * authenticate requests with JWT Bearer Strategy
+   * providers/BearerToken.ts
+   *
+   * 10. Authorisation, Soft Delete
+   * Emulate permissions recieved in the jwt header
+   * @authorisation decorator on testing routes
+   * Soft delete on models/user.model.ts
+   *
+   */
+  //
+  //
+
   constructor(options: ApplicationConfig = {}) {
     super(options);
     // confirgure middleware
