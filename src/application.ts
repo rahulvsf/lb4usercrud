c7b614b973aee831a9479651a782c21232fa20cb
Rahul Vijay <rahul.vijay@sourcefuse.com>
2023-03-29 16:52:06 +0530

    Revert "pr-4"
    This reverts commit 0fbaabe13194575035e2ba7c39f4609b32250df3.

--- src/application.ts
+++ src/application.ts
@@ -13,6 +13,8 @@ import {
   AuthorizationComponent,
 } from 'loopback4-authorization';
 import path from 'path';
+import {LoggerComponent} from './components/logger';
+import {jwtMiddleware} from './middleware/jwtheader';
 import {BearerTokenVerifierProvider} from './providers/BearerToken';
 import {MySequence} from './sequence';
 
@@ -23,6 +25,8 @@ export class UserappApplication extends BootMixin(
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
+    // confirgure middleware
+    this.middleware(jwtMiddleware);
 
     // Set up the custom sequence
     this.sequence(MySequence);
@@ -35,6 +39,8 @@ export class UserappApplication extends BootMixin(
       path: '/explorer',
     });
     this.component(RestExplorerComponent);
+    // initialize custom winston logger component
+    this.component(LoggerComponent);
     // initialize Auth component from loopback
     this.component(AuthenticationComponent);
 
--- src/sequence.ts
+++ src/sequence.ts
@@ -3,6 +3,7 @@ import {
   FindRoute,
   HttpErrors,
   InvokeMethod,
+  InvokeMiddleware,
   ParseParams,
   Reject,
   RequestContext,
@@ -19,11 +20,16 @@ import {
   AuthorizeFn,
   UserPermissionsFn,
 } from 'loopback4-authorization';
-
+import {LoggerComponentKeys, LogTypes} from './components/logger/logger.keys';
+import {LoggerFunction} from './components/logger/logger.types';
+import {jwtMiddleware} from './middleware/jwtheader';
 import {User} from './models';
 dotenv.config();
 
 export class MySequence implements SequenceHandler {
+  @inject(SequenceActions.INVOKE_MIDDLEWARE, {optional: true})
+  protected invokeMiddleware: InvokeMiddleware = () => false;
+
   constructor(
     @inject(SequenceActions.FIND_ROUTE)
     protected findRoute: FindRoute,
@@ -35,6 +41,8 @@ export class MySequence implements SequenceHandler {
     public send: Send,
     @inject(RestBindings.SequenceActions.REJECT)
     public reject: Reject,
+    @inject(LoggerComponentKeys.CUSTOM_LOGGER_FN)
+    public logger: LoggerFunction,
     @inject(AuthenticationBindings.USER_AUTH_ACTION)
     protected authenticateRequest: AuthenticateFn<User>,
     @inject(AuthorizationBindings.USER_PERMISSIONS)
@@ -46,6 +54,11 @@ export class MySequence implements SequenceHandler {
   async handle(context: RequestContext): Promise<void> {
     try {
       const {request, response} = context;
+
+      const finished = await this.invokeMiddleware(context, {
+        middlewareList: [jwtMiddleware],
+      });
+      if (finished) return;
 
       const route = this.findRoute(request);
       const args = await this.parseParams(request, route);
@@ -67,11 +80,41 @@ export class MySequence implements SequenceHandler {
       if (!isAccessAllowed) {
         throw new HttpErrors.Forbidden(AuthorizeErrorKeys.NotAllowedAccess);
       }
+      this.logStarting(context);
 
       const result = await this.invoke(route, args);
       this.send(response, result);
+      this.logEnding();
     } catch (error) {
+      this.logError();
       this.reject(context, error);
     }
+  }
+
+  private log(message: string, level?: number): void {
+    this.logger(level ?? LogTypes.INFO, message);
+  }
+
+  private logStarting(context: RequestContext): void {
+    const startDate = new Date();
+    this.log('\n************ STARTING **********');
+    this.log(startDate.toLocaleTimeString());
+    this.log('HOST: ' + context.request.headers.host);
+    this.log('REFERER: ' + context.request.headers.referer);
+    this.log('USER AGENT: ' + context.request.headers['user-agent']);
+    this.log('IP: ' + context.request.socket.remoteAddress);
+  }
+
+  private logEnding() {
+    const endDate = new Date();
+    this.log('CLOSING AT ' + endDate.toLocaleTimeString());
+  }
+
+  private logError() {
+    const errorTime = new Date();
+    this.log(
+      'Error Occured at - ' + errorTime.toLocaleTimeString(),
+      LogTypes.ERROR,
+    );
   }
 }
